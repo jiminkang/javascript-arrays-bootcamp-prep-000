af249a507fdffee02546fb555be80f3516e939a9
Jimin Kang <jimin.kang91@gmail.com>
2017-12-18 03:01:01 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,6 +1,6 @@
 var chocolateBars =["snickers" , "hundred grand","kitkat", "skittles"]
 function addElemantToBeginningofArray (arr, ele){
-  
+  arr = [ele, ...arr]
 }
 
 function destructivelyAddElementToBeginningofArray (arr,ele){
